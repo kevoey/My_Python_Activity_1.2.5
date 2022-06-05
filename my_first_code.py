1c1632d7e302c9a0b95b772f5db290633abc40fa
kevoey <103943927+kevoey@users.noreply.github.com>
2022-06-05 17:31:46 +0700

    Added 2 files Jupyter Notebook - results (1.2.5)
    Added 2 files Jupyter Notebook - results from practical activity1.2.5, course 2

---
+++ my_first_code.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # 1.2.5 Writing Python Code - My_first_code
+
+# In[1]:
+
+
+# this is my first line of code
+
+print("My name is Kevin Wijaya Oey")
+print("The current date is 5 June 2022")
+
+
+# In[2]:
+
+
+# Write a program to analyse real estate data
+
+a = 45000
+b = 23400
+c = 67000
+d = 34600
+e = 12900
+
+#Total Sales for December (Lindstrom & Thomas)
+
+print (a+b+c+d+e)
+
+
+# In[3]:
+
+
+# average sales pprice for December, where average = total sales / number of sales
+
+print((a+b+c+d+e)/5)
+
+
+# In[4]:
+
+
+# Property price - property C, where sales = property price + commission
+# Property C's commision is 6,500
+
+print(c-6500)
+
